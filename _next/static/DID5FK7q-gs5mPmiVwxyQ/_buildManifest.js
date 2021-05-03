b97d325e61452b90af4b5e7d56ed5aad4c467684
mariocesar <mariocesar@users.noreply.github.com>
2021-05-03 06:00:15 +0000

    deploy: 83f692e88b3bf26beb958c85a653f863c0facab7

---
+++ _next/static/DID5FK7q-gs5mPmiVwxyQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4e209ef2d8f3044de3e2.js"],"/_error":["static/chunks/pages/_error-cfa7a753384620244b83.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Zs8bMDBduljRfsV9kGuv7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8e7aa1f550d70d82c15d.js"],"/_error":["static/chunks/pages/_error-cfa7a753384620244b83.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
