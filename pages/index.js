06db7ab5016bec9d1dbd9a42542c76aaebb423f2
Mario César <mariocesar.c50@gmail.com>
2021-05-03 02:28:06 -0400

    Headers

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,9 @@ const ListItem = ({ icon, children }) => {
 };
 
 const MarkupLink = ({ href }) => {
+  let cleanHref = href.startsWith("http") ? href : `https://${href}`;
   return (
-    <a target="_blank" href={href}>
+    <a target="_blank" href={cleanHref}>
       {href}
     </a>
   );
@@ -19,13 +20,35 @@ const MarkupLink = ({ href }) => {
 export default function Home() {
   return (
     <>
-      <Head>
+      <Head itemtype="http://schema.org/Blog" itemscope="">
+        <meta charset="utf-8" />
         <title>Mario-César Señoranis</title>
-        <meta name="viewport" content="width=device-width"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="I’m a software developer working at Zapier.com, here is my Personal site and Blog."
+        />
+        <meta
+          name="keywords"
+          content="python, bolivia, mariocesar, mariocesar_bo, zapier, software engeineer"
+        />
+        <meta name="author" content="Mario-César Señoranis" />
+
+        <link rel="canonical" itemprop="url" href="https://mariocesar.xyz/" />
+        <link rel="author" href="https://mariocesar.xyz" />
+
+        <meta property="og:title" content="" />
+        <meta property="og:url" content="https://mariocesar.xyz" />
+        <meta
+          property="og:site_name"
+          itemprop="name"
+          content="Mario-César Señoranis Personal Website &amp; Blog"
+        />
+        <meta property="og:type" content="blog" />
       </Head>
       <header></header>
-      <main>
+      <main role="main">
         <div className="container">
           <h1>
             👋🏼 Hello,
@@ -35,44 +58,52 @@ export default function Home() {
           <p>
             I’m a software developer, trying to write, create, and mostly being
             a great father. I work at ⚡️
-            <MarkupLink href="https://zapier.com/" /> as an Integration Software
-            Engineer. You’ve found my slice of the internet.
+            <MarkupLink href="zapier.com" /> as an Integration Software Engineer
+            a job that I love. I also spend some time in{" "}
+            <MarkupLink href="humanzilla.com" /> a two-person software agency
+            with my Wife.
           </p>
+          <p>I grew up, live and work from Santa Cruz de la Sierra, Bolivia.</p>
+          <p>And you’ve found my slice of the internet.</p>
           <p>You can also find me:</p>
           <ul role="list">
             <ListItem icon="🧑🏽‍💻">
               Talking about work in LinkedIn{" "}
-              <MarkupLink href="https://www.linkedin.com/in/mariocesar/" />
+              <MarkupLink href="linkedin.com/in/mariocesar/" />
             </ListItem>
 
             <ListItem icon="📸">
               Sharing pretty photos at Instagram{" "}
-              <MarkupLink href="https://instagram.com/mariocesar_bo/" />
+              <MarkupLink href="instagram.com/mariocesar_bo/" />
             </ListItem>
             <ListItem icon="🎉">
               Speaking in Clubhouse{" "}
-              <MarkupLink href="https://www.joinclubhouse.com/@mariocesar" />
+              <MarkupLink href="joinclubhouse.com/@mariocesar" />
               <ul role="list">
                 <ListItem icon="☕">
                   I host a room in the club "Club del Desayuno" every day at 9
                   am about Accountability and Networking. See{" "}
-                  <MarkupLink href="https://www.joinclubhouse.com/club/club-del-desayuno" />
+                  <MarkupLink href="joinclubhouse.com/club/club-del-desayuno" />
                 </ListItem>
               </ul>
             </ListItem>
             <ListItem icon="👾">
               Sharing code in&nbsp;
-              <MarkupLink href="https://github.com/mariocesar" />, and projects
-              like this website&nbsp;
-              <MarkupLink href="https://github.com/mariocesar/mariocesar" />
+              <MarkupLink href="github.com/mariocesar" />, and projects like
+              this website&nbsp;
+              <MarkupLink href="github.com/mariocesar/mariocesar" />
               <ul role="list">
                 <ListItem icon="📝">
                   <span>
                     I have random and sometimes worthy bits of code unordered in{" "}
-                    <MarkupLink href="https://gist.github.com/mariocesar" />
+                    <MarkupLink href="gist.github.com/mariocesar" />
                   </span>
                 </ListItem>
               </ul>
+            </ListItem>
+            <ListItem icon="🐦">
+              Saying little in Twitter{" "}
+              <MarkupLink href="twitter.com/mariocesar_bo" />
             </ListItem>
           </ul>
         </div>
