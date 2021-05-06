9270b080b2439dfbe0bac6711c223de4adbe56db
mariocesar <mariocesar@users.noreply.github.com>
2021-05-06 17:11:31 +0000

    deploy: ce6d5a2f1c192b187826c61c67e543170b725cfd

--- _next/static/sUHombFz-ggAzTBwaMEvr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3ff0eb20be776abd5d99.js"],"/_error":["static/chunks/pages/_error-cfa7a753384620244b83.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
